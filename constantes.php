a81989be283e6ecd073a0623e821472073a7c08f
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-04-02 10:52:20 -0300

    constantes

---
+++ constantes.php
@@ -0,0 +1,12 @@
+<?php
+
+define("ESCOLA", "Treinaweb Cursos"); // Função para definição de constante
+                                      // Primeiro parâmetro: NOME_DA_CONSTANTE
+                                      // Segundo parâmetro: VALOR DA CONSTANTE
+                                      // Pode ser usada dentro e fora de escopo
+
+echo ESCOLA;
+
+const CURSO = "PHP"; // Outra forma de definir constantes, mas só pode ser usada fora de escopo
+
+echo CURSO;
