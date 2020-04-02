04fc859f967b1c02e54d4f816fdbf5341a1c2883
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-04-02 10:52:11 -0300

    variaveis

---
+++ variaveis.php
@@ -0,0 +1,16 @@
+<?php
+echo "<br>";
+$email = "jamersonalsilva14@gmail.com";
+echo $email;
+
+echo "<br>";
+$email = "jamersonaguiar@alu.ufc.br";
+echo $email;
+
+echo "<br>";
+$email = 100;
+echo $email;
+
+echo "<br>";
+$nome = "Jamerson Aguiar";
+echo $nome;
